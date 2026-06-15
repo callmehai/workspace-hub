d7f629c856ce0fe7c5c1ef1c58bd1264e55dfe54
Dunghthe04 <tuandungyz09@gmail.com>
2026-06-16 01:10:08 +0700

    feat(calendar): implement calendar sync service.

---
+++ backend/src/WorkspaceHub.Application/Interfaces/Services/ICalendarSyncService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkspaceHub.Application.Abstractions;
+using WorkspaceHub.Domain.Entities;
+
+namespace WorkspaceHub.Application.Interfaces.Services
+{
+    public interface ICalendarSyncService
+    {
+        Task<SyncResult> SyncAsync(Guid connectionId, Guid userId, CancellationToken ct = default);
+        Task<SyncResult> SyncConnectionAsync(Connection connection, CancellationToken ct = default);
+    }
+}
