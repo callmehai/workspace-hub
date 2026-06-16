3aeea78126f01ed3b5730f1d7b97e28d23a5def1
Dunghthe04 <tuandungyz09@gmail.com>
2026-06-16 07:22:20 +0700

    feat(drive): implement drive sync service.

---
+++ backend/src/WorkspaceHub.Application/Services/IDriveSyncService.cs
@@ -0,0 +1,10 @@
+﻿using WorkspaceHub.Application.Abstractions;
+using WorkspaceHub.Domain.Entities;
+
+namespace WorkspaceHub.Application.Interfaces.Services;
+
+public interface IDriveSyncService
+{
+    Task<SyncResult> SyncAsync(Guid connectionId, Guid userId, CancellationToken ct = default);
+    Task<SyncResult> SyncConnectionAsync(Connection connection, CancellationToken ct = default);
+}
