dffd97b20da88d098b24cdacbabe9ca2d03f5971
callmehai <108572472+callmehai@users.noreply.github.com>
2026-07-07 10:10:52 +0700

    feat(jira): bật Atlassian integration mặc định (migration EnableJiraIntegration) (#78)

--- backend/src/WorkspaceHub.Infrastructure/Data/Migrations/20260707025620_EnableJiraIntegration.Designer.cs
+++ backend/src/WorkspaceHub.Infrastructure/Data/Migrations/20260707025620_EnableJiraIntegration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkspaceHub.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WorkspaceHub.Infrastructure.Data;
 namespace WorkspaceHub.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260707025620_EnableJiraIntegration")]
+    partial class EnableJiraIntegration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/WorkspaceHub.Infrastructure/Data/Migrations/20260707025620_EnableJiraIntegration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkspaceHub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnableJiraIntegration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Integrations",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "IsEnabled",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Integrations",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "IsEnabled",
+                value: false);
+        }
+    }
+}
